0690f3a9605a6b27747e546d01bb25c0878bddb5
munyeongkim2 <wgansdud2@naver.com>
2024-05-01 23:49:31 +0900

    day4 add

--- day3/src/main/java/com/nhnacademy/day3/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.day3;
-
-public enum Gender {
-    M,F
-}
--- day3/src/main/java/com/nhnacademy/day3/controller/Command.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacademy.day3.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Command {
-    String execute(HttpServletRequest request, HttpServletResponse response);
-}
